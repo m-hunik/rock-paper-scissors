ddaa0caee5cef4d4f98dd194d832ba572c3dabe7
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2024-12-10 17:07:08 +0100

    Allow player input and return the choice

--- rps.js
+++ rps.js
@@ -1,14 +1,30 @@
-function getComputerChoice (max) {
-    let random = Math.floor(Math.random() * max);
+// function getComputerChoice (max) {
+//     let random = Math.floor(Math.random() * max);
 
-    if (random == 1) {
-        return "rock";
-    } else if (random == 2) {
-        return "paper";
+//     if (random == 1) {
+//         return "rock";
+//     } else if (random == 2) {
+//         return "paper";
+//     } else {
+//         return "scissors";
+//     }
+// }
+
+// console.log(getComputerChoice(3));
+
+function getHumanChoice () {
+
+    let result = prompt("Rock, paper or scissors?");
+
+    if (result == "rock") {
+        return result;
+    } else if (result == "paper") {
+        return result;
+    } else if (result == "scissors") {
+        return result;
     } else {
-        return "scissors";
+        return "Only r, p or s!"
     }
 }
 
-console.log(getComputerChoice(3));
-
+console.log(getHumanChoice());
