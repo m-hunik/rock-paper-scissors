e7081b8eeaf1e9252ad271c7bb6599f0e804aff6
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2024-12-11 20:03:22 +0100

    Apply game logic

--- rps.js
+++ rps.js
@@ -16,21 +16,34 @@ function getHumanChoice() {
 function getComputerChoice(max) {
     let random = Math.floor(Math.random() * max);
     
-    if (random == 1) {
+    if (random == 0) {
         return "rock";
-    } else if (random == 2) {
+    } else if (random == 1) {
         return "paper";
     } else {
         return "scissors";
     }
-
 }
 
 function playRound (humanChoice, computerChoice) {
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice(3);
-    
-    console.log(humanChoice, computerChoice);
+
+    console.log(`You chose:  ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+
+    if (humanChoice == computerChoice) {
+        console.log("Draw!")
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log("You win!");
+    } else {
+        console.log("You lose!");
+    }
+
 }
 
 playRound();
