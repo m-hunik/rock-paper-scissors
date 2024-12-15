87128e2ef2cc9f837206384947a9794ca7450139
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2024-12-15 15:03:11 +0100

    Update score correctly
    Earlier version would reset the score after every round

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+
 function getHumanChoice() {
 
     let result = prompt("Rock, paper or scissors?");
@@ -25,42 +26,42 @@ function getComputerChoice(max) {
     }
 }
 
-function playGame() {
-
-    let roundCounter = 0;
-
-    function playRound (humanChoice, computerChoice) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice(3);
+function playRound (humanChoice, computerChoice) {
     
-        let humanScore = 0;
-        let computerScore = 0;
-
-        console.log(`You chose:  ${humanChoice}`);
-        console.log(`Computer chose: ${computerChoice}`);
+    console.log(`You chose:  ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
     
-        if (humanChoice == computerChoice) {
-            console.log("Draw!")
-        } else if (
-            (humanChoice == "rock" && computerChoice == "scissors") ||
-            (humanChoice == "paper" && computerChoice == "rock") ||
-            (humanChoice == "scissors" && computerChoice == "paper")
-        ) {
-            console.log("You win!");
-            humanScore += 1;
-        } else {
-            console.log("You lose!");
-            computerScore += 1;
-        }
+    if (humanChoice == computerChoice) {
+        console.log("Draw!")
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log("You win!");
+        return "human"
+    } else {
+        console.log("You lose!");
+        return "computer"
+    }
+}
 
+function playGame(rounds = 5) {
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for(let i = 0; i < rounds; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice(3);
+        let result = playRound(humanChoice, computerChoice);
+
+        if (result == "human") {humanScore++}
+        else if (result == "computer") {computerScore++}
+        
         console.log("Human: " + humanScore + " " + "Computer: " + computerScore);
     }
-    playRound();
-    return roundCounter += 1;
 }
 
-let scoreKeeper = playGame();
-for (let i = scoreKeeper; i < 5; i++) {
-    playGame();
-}
+playGame();
 
